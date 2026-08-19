631e41dcf651e83038f1d9d1553de72fcb1d6efc
BlackBearReloaded <blackbearreloaded@protonmail.com>
2026-08-18 23:49:01 -0400

    Include TLS smoke test in core suite

--- tests/core_suite.py
+++ tests/core_suite.py
@@ -31,6 +31,7 @@ TEST_FILES = (
     "test_dns.py",
     "test_core_modules.py",
     "test_ssl_hashlib.py",
+    "test_tls_handshake.py",
     "test_thread_context.py",
     "test_concurrency.py",
     "test_data_formats.py",
